02f4d7355a19eb84b72486bd32f90478d451e897
Rambash <Rambash@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-05 21:09:25 +0000

    Fixing some bugs
    git-svn-id: https://plugins.svn.wordpress.org/panoramio-images/trunk@160763 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/pamjax.js
+++ js/pamjax.js
@@ -1,12 +1,17 @@
 jQuery(document).ready(function() {
 	 $("#pamore").click(function () { 
-		$.getJSON("http://www.panoramio.com/map/get_panoramas.php?order=popularity&set=public&from=0&to=6&minx=17.872631549835205&miny=59.317929544812046&maxx=17.889862060546875&maxy=59.322965844339194&size=square&format=json&callback=?",
+		start = start * 1;
+		end = end * 1;
+
+		start = start+6;
+		end = end+6;
+		$.getJSON("http://www.panoramio.com/map/get_panoramas.php?order=popularity&set=public&from=" + start + "&to=" + end + "&minx=" + minx + "&miny=" + miny + "&maxx=" + maxx + "&maxy=" + maxy + "&size=square&format=json&callback=?",
 			function(data)
 			{
 				$("#pamdiv").replaceWith('<div id="pamdiv"></div>')
 				$.each(data.photos, function(i,item)
 					{
-					$("#pamdiv").append('<a href="http://www.panoramio.com/photo' + $(this)[0].photo_id  + '"><img src=' + $(this)[0].photo_file_url + ' style="border:1px solid gray;margin-bottom:5px;margin:1px;"></a>');
+					$("#pamdiv").append('<a href="http://www.panoramio.com/photo/' + $(this)[0].photo_id  + '"><img src="' + $(this)[0].photo_file_url + '" title="' + $(this)[0].photo_title + '" alt="' + $(this)[0].photo_title  + '" style="border:1px solid gray;margin-bottom:5px;margin:1px;"></a>');
 			});
 		});
 	});
