a2cf7b3f491b157634ca43c4aed46fa0bed1684f
Rambash <Rambash@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-08 00:44:12 +0000

    Tagging version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/panoramio-images/tags/1.3@161445 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/pamjax.js
+++ js/pamjax.js
@@ -1,11 +1,31 @@
+if (window.attachEvent) {window.attachEvent('onload', retrieve);}
+else if (window.addEventListener) {window.addEventListener('load', retrieve, false);}
+else {document.addEventListener('load', retrieve, false);}
+
+function retrieve(){
+start = start * 1;
+end = end * 1;
+inc = end;
+
+$.getJSON("http://www.panoramio.com/map/get_panoramas.php?order=popularity&set=public&from=" + start + "&to=" + end + "&minx=" + minx + "&miny=" + miny + "&maxx=" + maxx + "&maxy=" + maxy + "&size=" + size + "&format=json&callback=?",
+			function(data)
+			{
+				$("#pamdiv").replaceWith('<div id="pamdiv"></div>')
+				$.each(data.photos, function(i,item)
+					{
+					$("#pamdiv").append('<a href="http://www.panoramio.com/photo/' + $(this)[0].photo_id  + '"><img src="' + $(this)[0].photo_file_url + '" title="' + $(this)[0].photo_title + '" alt="' + $(this)[0].photo_title  + '" style="border:1px solid gray;margin-bottom:5px;margin:1px;"></a>');
+			});
+		});
+		
+} 
+
 jQuery(document).ready(function() {
 	 $("#pamore").click(function () { 
-		start = start * 1;
-		end = end * 1;
 
-		start = start+6;
-		end = end+6;
-		$.getJSON("http://www.panoramio.com/map/get_panoramas.php?order=popularity&set=public&from=" + start + "&to=" + end + "&minx=" + minx + "&miny=" + miny + "&maxx=" + maxx + "&maxy=" + maxy + "&size=square&format=json&callback=?",
+
+		start = start+inc;
+		end = end+inc;
+		$.getJSON("http://www.panoramio.com/map/get_panoramas.php?order=popularity&set=public&from=" + start + "&to=" + end + "&minx=" + minx + "&miny=" + miny + "&maxx=" + maxx + "&maxy=" + maxy + "&size=" + size + "&format=json&callback=?",
 			function(data)
 			{
 				$("#pamdiv").replaceWith('<div id="pamdiv"></div>')
