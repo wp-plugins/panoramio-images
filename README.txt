618c5696a2f25fb28579673c82f5e623514bc828
Rambash <Rambash@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-08 00:43:26 +0000

    Updating to version 1.3 - See changelog
    git-svn-id: https://plugins.svn.wordpress.org/panoramio-images/trunk@161444 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
 ﻿=== Panoramio Images ===
-Contributors: rambash
+Contributors: Rambash
 Donate link: -
-Tags: panoramio, api, images
+Tags: panoramio, api, images, widget, sidebar
 Requires at least: -
 Tested up to: -
 Stable tag: trunk
@@ -10,116 +10,30 @@ A wordpress plugin for retrieving images and values through the Panoramio API.
 
 == Description ==
 
-A wordpress plugin for retrieving images and values through the Panoramio API.
+A wordpress widget that retrieve images from Panoramio API.
 
+HOW TO
+
+To display the widget, add a custom field to a post called "panoramio". The value of panoramio should be the coordinate to the area that you want to display pictures from.
+The value should be formatted like this: X:Y
+
+It is also possible to supply the script with two coordinates, for a more precise area. Use this format: minx:miny:maxx:maxy  
+
+All coordinates should be given in the decimal form of latitude/longitude. I recommend using http://mapki.com/getLonLat.php.
+
+CHANGELOG
+1.3:
+-Added AJAX support
+-Converted the script into a widget
+-Reduced the code
+-Fixed some bugs
 
 == Installation ==
-Just copy the files to your wordpress plugins directory and activate it through the admin interface.
+1. Unzip panoramio-images.zip
+2. Place the panoramio-images folder into /wp-content/plugins/
+3. Activate the plugin in the admin panel
+4. Navigate to widget settings page, drag the widget into your sidebar and set your preferences
 
 == Screenshots ==
 
 1. The output from pam_display
-
-== FUNCTIONS ==
-pam_get($start, $end, $ll, $size, $var)
-
-	* returns $var-values into an array
-
-pam_show($start, $end, $ll, $size)
-
-	* displays images
-
-pam_showfromcenter($start, $end, $centercoord, $radius, $size)
-
-	* creates a rectangle around $centercoord with the sides being $radius in km
-
-pam_display($start, $end, $coordinate, $size, $radius = 0.5)
-
-	* Checks if you've entered one coordinate or two coordinates and then retrieves the images.
-
-== VARIABLES ==
-
-$start
-
-	* Integer value that decides where panoramio should start retrieving images.
-
-$end	
-
-	* Integer value that decides where panoramio should stop retrieving images.
-
-$size
-
-	* medium
-	* small
-	* thumbnail
-	* square
-	* mini_square
-
-$var
-
-	  Name			  Ex
-	* photo_id Ex     505229
-	* photo_title     Etangs près de Dijon"
-	* photo_url       http://www.panoramio.com/photo/505229"
-	* photo_file_url  http://static2.bareka.com/photos/medium/505229.jpg"
-	* longitude       5.168552
-	* latitude        47.312642
-	* width           350
-	* height          500
-	* upload_date     20 January 2007
-	* owner_id        78506 
-	* owner_name      Philippe Stoop
-	* owner_url       http://www.panoramio.com/user/78506
-
-
-$ll
-
-	* Top left and bottom right coordinates for the rectangle. The coordinates should be the latitudes/longitudes decimal form and formatted like this: minx:miny:maxx:maxy. 
-
-
-$centercoord
-
-	* Single coordinate. The coordinates should be the latitudes/longitudes decimal form and formatted as this: x:y
-
-$radius
-
-	* It defines the length in km of the sides of the box created in pam_showfromcenter
-
-
-== EXAMPLES ==
-
-
-1: Show the five first thumbnails from coordinates given in a posts custom field called "panoramio".
-
-        * pam_show(0,5, get_post_meta($post->ID, "panoramio", true), "thumbnail");
-
-
-2: Retrieving a specific variable from the five first thumbnails and placing it into an array.
-
-
-        * pam_get(0, 5, 17.872631549835205:59.317929544812046:17.889862060546875:59.322965844339194, "thumbnail", "owner_name")
-
-3: Show six thumbnails in an area of 500x500m around a coordinate
-
-
-	* pam_showfromcenter(0, 6, "17.881622314453125:59.320666756687736", 0.5, "square");
-
-4: Show six thumbnails. If two points are given then it retrieves the images from that rectangle. If only a single point is given then it creates a 500x500m rectangle and checks for images.
-
-
-	* pam_display(0, 6, 17.872631549835205:59.317929544812046, "square", 0.5);
-
-== WIDGET ==
-
-A proper widget is under development. Until then you can put this in your sidbar.
-
-		<?php
-		pam_widget(get_post_meta($post->ID, "panoramio", true));
-		?>
-
-It will show a box with six images on all posts with a custom field called "panoramio". The fields value should be the coordinates to the point where
-images should be retrieved from.
-
-== MISC ==
-
-All coordinates should be given in the decimal form of latitude/longitude. I recommend using http://mapki.com/getLonLat.php.
--- pam.php
+++ pam.php
@@ -3,9 +3,9 @@
 Plugin Name: Panoramio Images
 Plugin URI: http://www.letsgeek.com/plugins/panoramio-images/
 Description: Adds functions to retrieve values and images from panoramio.
-Version: 1.2
+Version: 1.3
 Author: Rambash
-Author URI: http://www.letsgeek.com/plugins/panoramio-images/
+Author URI: http://www.letsgeek.com
 
 Copyright 2009  Rambash (email : rambash@letsgeek.com)
 
@@ -22,23 +22,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-FUNCTIONS
-	
-	pam_get
-	-Retrieves certain variables
-
-	pam_show
-	-Shows a set of images
-
-	pam_showfromcenter
-	-Shows a set of images based on one point
-
-	pam_display
-	-Checks if you've entered one coordinate or two coordinates and then retrieves the images.
-
-HOWTO
-Read the readme.txt
 */
 
 add_action('wp_head', 'snoppinit', 0);
@@ -49,112 +32,135 @@ wp_enqueue_script( 'pamjax', WP_PLUGIN_URL . '/panoramio-images/js/pamjax.js', a
 
 }
 
-function pam_get($start, $end, $ll, $size, $var){
-
-	$ll = explode(":",$ll);
-	$url = 'http://www.panoramio.com/map/get_panoramas.php?order=popularity&set=public&from='.$start.'&to='.$end.'&minx='.$ll[0].'&miny='.$ll[1].'&maxx='.$ll[2].'&maxy='.$ll[3].'&size='.$size;
-     
-	$session = curl_init($url);
-
-    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($session, CURLOPT_FAILONERROR, true);
-    curl_setopt($session, CURLOPT_CONNECTTIMEOUT, 5);
-    curl_setopt($session, CURLOPT_TIMEOUT, 10);
-
-    $stream = curl_exec($session);
-    curl_close($session);
-    $value = json_decode($stream, true);
-	
-	$arr = array();
-	for ($i = $start; $i < $end; $i++)
+function widget_pam_show($coordinate, $args)
 	{
-	 array_push($arr, $value["photos"][$i][$var]);
-	}
 
-	return $arr;
-}
-
-function pam_show($start, $end, $ll, $size){
-
-	//If you want to change the output, edit here.
-	if($ll)
-		{
-			echo '
-			<div style="background-color:#ffffcd;padding:6px;border:1px solid #d7d8b9;margin-top:10px;">
-				
-					<center>
-						<b>Area pictures</b><br />
-						<div id="pamdiv">';
-						$newstring =strstr ($string, "Boston");
-						$arr = pam_get($start ,$end, $ll, $size, "photo_file_url");
-
-						$ar = $arr;
-
-							for($i=$start; $i<$end; $i++)
-								{
-									$ar[$i] = rtrim(strrchr($ar[$i], "/"), ".jpg");
-								}	
-						
-
-						for($i=$start; $i<$end; $i++)
-							{
-								echo '<a href="http://www.panoramio.com/photo'.$ar[$i].'"><img src='.$arr[$i].' style="border:1px solid gray;margin-bottom:5px;margin:1px;"></a>';
-							}		
-					echo '</div>
-					<p style="text-align:right;font-size: 85%;margin-right:1px;"><a href="#" id="pamore">See more photos</a></p>
-					<p id="panoramio" style="color:#A1A1A1;font-size:85%;margin-top:3px;">Photos provided by <a href="http://www.panoramio.com">Panoramio</a> are under the copyright of their owners.</p></center>
-			</div>';
-		echo "<!-- plugin made by rambash - http://www.letsgeek.com/plugins/panoramio-images/ -->";
-		$ll = explode(":",$ll);
-		?>
-		<script>
-		var maxx = "<?= $ll[2] ?>";
-		var maxy = "<?= $ll[3] ?>";
-		var minx = "<?= $ll[0] ?>";
-		var miny = "<?= $ll[1] ?>";
-		var start = "<?= $start ?>";
-		var end = "<?= $end ?>";
-		</script>
-		<?
-
-		}
-}
-
-function pam_showfromcenter($start, $end, $centercoord, $radius, $size){
-
-		//I have NO idea how accurate this is.
-		list($long, $lat) = split(':', $centercoord);
-
-		$maxx = $long + ((($radius*0.5) / 1.8553) *  0.016666666666666666666666666666667);
-		$maxy = $lat + ((($radius*0.5) / 1.84293) *  0.016666666666666666666666666666667);
-		$minx = $long - ((($radius*0.5) / 1.8553) *  0.016666666666666666666666666666667);
-		$miny = $lat - ((($radius*0.5) / 1.84293) *  0.016666666666666666666666666666667);
-
-		pam_show($start, $end, "$minx:$miny:$maxx:$maxy", $size);
+			global $wpdb;
+			extract( $args );
+			$options =  get_option('widget_pam');
+			$title = $options['title'];
 		
-}
-
-function pam_display($start, $end, $coordinate, $size, $radius = 0.5){
-
-			If(substr_count($coordinate, ":")>1)
+			If(substr_count($coordinate, ":")==1)
 			{
-				pam_show($start, $end, $coordinate, $size);
-				}
-				else
-				{
-				pam_showfromcenter($start, $end, $coordinate, $radius, $size);
-			}
+				//IF one coordinate
+				
+				//I have NO idea how accurate this is.
+				list($long, $lat) = split(':', $centercoord);
+
+				$maxx = $long + ((($radius*0.5) / 1.8553) *  0.016666666666666666666666666666667);
+				$maxy = $lat + ((($radius*0.5) / 1.84293) *  0.016666666666666666666666666666667);
+				$minx = $long - ((($radius*0.5) / 1.8553) *  0.016666666666666666666666666666667);
+				$miny = $lat - ((($radius*0.5) / 1.84293) *  0.016666666666666666666666666666667);
+				
+				?>
+					<script>
+						var maxx =  "<?= $maxx ?>";
+						var maxy =  "<?= $maxy ?>";
+						var minx  =  "<?= $minx ?>";
+						var miny  =  "<?= $miny ?>";
+						var start  = 0;
+						var end   = <?= $options['quantity'] ?>;
+						var size   = "<?= $options['size']?>";
+					</script>
+				<?
+				
+			}else{
+			//IF two coordinates
+			$ll = explode(":",$coordinate);
+			
+			?>
+				<script>
+					var maxx = "<?= $ll[2] ?>";
+					var maxy = "<?= $ll[3] ?>";
+					var minx = "<?= $ll[0] ?>";
+					var miny = "<?= $ll[1] ?>";
+					var start = 0;
+					var end = <?= $options['quantity'] ?>;
+					var size = "<?= $options['size']?>";
+			</script>
+			<?
+	}
+		
+	echo '<a name="pam"></a>
+			<center>
+				<b>'. $before_title . $title . $after_title .'</b><br />
+				<div>
+					<div id="pamdiv">Loading images...</div>
+					<p style="text-align:right;font-size: 85%;margin-right:1px;"><a href="#pam" id="pamore">See more photos</a></p>
+				</div>
+				<p id="panoramio" style="color:#A1A1A1;font-size:85%;margin-top:3px;">Photos provided by <a href="http://www.panoramio.com">Panoramio</a> are under the copyright of their owners.</p>
+			</center>';	
 }
 
-function pam_widget($postmeta)
-{
-	if(is_single())
-	{
-		if($postmeta){
-			pam_display(0, 6, $postmeta, "square");
+function widget_pam_control() {
+
+		$options = $newoptions = get_option('widget_pam');
+	
+		if ( $_POST["pam-submit"] ) {
+		$newoptions['title'] = strip_tags(stripslashes($_POST['pam-title']));
+		$newoptions['size'] = $_POST['pam-size'];
+		$newoptions['quantity'] = (int)$_POST['pam-quantity'];
+		}
+		
+		if ( $options != $newoptions ) {
+			$options = $newoptions;
+			update_option('widget_pam', $options);
+		}
+	
+	?><label for="pam-title" style="line-height:25px;display:block;"><?php _e('Widget title:', 'widgets'); ?> <input style="width: 200px;" type="text" id="pam-title" name="pam-title" value="<?php echo ($options['title'] )?>" /></label>
+			<label for="pam-size" style="line-height:25px;display:block;">
+				<?php _e('Image size:', 'widgets'); ?>
+					<select style="width: 200px;" id="pam-size" name="pam-size">
+						<option value="mini_square"<?php if ($options['size'] == 'mini_square') echo ' selected' ?>>Mini square</option>	
+						<option value="square"<?php if ($options['size'] == 'square') echo ' selected' ?>>Square</option>
+						<option value="thumbnail"<?php if ($options['size'] == 'thumbnail') echo ' selected' ?>>Thumbnail</option>
+					</select>
+			</label>
+			<label for="pam-quantity" style="line-height:25px;display:block;">
+				<?php _e('Quantity:', 'widgets'); ?>
+					<select style="width: 200px;" id="pam-quantity" name="pam-quantity"/>
+						<?php for($cnt=1;$cnt<=10;$cnt++): ?>
+							<option value="<?php echo $cnt ?>"<?php if((int)$cnt == $options['quantity']) echo ' selected' ?>><?php echo $cnt ?></option>
+						<?php endfor; ?>
+					</select>
+			</label>			
+			<input type="hidden" name="pam-submit" id="pam-submit" value="1" /><?
+}
+
+function widget_pam_init() {
+
+	// Check for the required API functions
+	if ( !function_exists('register_sidebar_widget') || !function_exists('register_widget_control') )
+		return;
+
+    $data = get_option('widget_pam');
+    if ( ! get_option('widget_pam')){
+    $data = array( 'title' => 'Panoramio Images' ,'size' => 'square', 'quantity' => 6);
+      add_option('widget_pam' , $data);
+    } 
+    
+	// This prints the widget
+	function widget_pam($args) {
+	
+	global $wp_query;
+	
+		if(get_post_meta($wp_query->post->ID, "panoramio", true))
+		 {
+		 	extract( $args );
+			echo $before_widget;
+			echo "\n".'<!-- Panoramio Images: START -->'."\n";
+			echo widget_pam_show(get_post_meta($wp_query->post->ID, "panoramio", true), $args);
+			echo "\n".'<!-- Widget made by Rambash - http://www.letsgeek.com -->'."\n";
+			echo "\n".'<!-- Panoramio Images: END -->'."\n";
+			echo $after_widget;
 		}
 	}
+
+	// Tell Dynamic Sidebar about our new widget and its control
+	register_sidebar_widget(array('Panoramio Images', 'widgets'), 'widget_pam');
+	register_widget_control(array('Panoramio Images', 'widgets'), 'widget_pam_control');
 }
 
-
+// Delay plugin execution to ensure Dynamic Sidebar has a chance to load first
+add_action('widgets_init', 'widget_pam_init');
 ?>
